81f4b503b58a4fb13b15d0aaefece02fc6f125f0
ValtersSt <valters.steinblums@gmail.com>
2021-04-01 14:59:29 +0300

    First

--- src/test/java/com/testing/pageObjects/pages/HomePage.java
+++ src/test/java/com/testing/pageObjects/pages/HomePage.java
@@ -16,17 +16,18 @@ public class HomePage extends TopMenuSection {
   public static By ALL_PRODUCTS_TITLE = text("All Products");
   public static By SIMPLE_SNACK_BAR_PAGE_RELOAD_BUTTON = css("simple-snack-bar button");
   public static By SIMPLE_SNACK_BAR = css("simple-snack-bar span");
-  public static By ITEMS_PER_PAGE;
-  public static By OPTION_24;
-  public static By BLOCK_NAME;
-  public static By ADD_TO_BASKET_BUTTON;
-  public static By PRODUCT_INFO_BLOCK;
-  public static By PRODUCT_INFO_PRICE;
-  public static By PRODUCT_INFO_CLOSE;
-  public static By REVIEWS_EXPAND;
-  public static By COMMENT_SECTION;
-  public static By REVIEW_TEXT_FIELD;
-  public static By SUBMIT_BUTTON;
+  public static By ITEMS_PER_PAGE = css("mat-select[role='combobox']");
+  public static By OPTION_24 = xpath("//span[contains(text(), '24')]");
+  //public static By OPTION_24 = css("mat-select[id=mat-select-0]");
+  public static By BLOCK_NAME = css("div.item-name");
+  public static By ADD_TO_BASKET_BUTTON = css("button[aria-label='Add to Basket']");
+  public static By PRODUCT_INFO_BLOCK = css("mat-dialog-container[role='dialog']");
+  public static By PRODUCT_INFO_PRICE = css("p[class='item-price']");
+  public static By PRODUCT_INFO_CLOSE = css("button[aria-label='Close Dialog']");
+  public static By REVIEWS_EXPAND = css("mat-expansion-panel[aria-label='Expand for Reviews']");
+  public static By COMMENT_SECTION = css("div[role='region']");
+  public static By REVIEW_TEXT_FIELD = css("textarea[aria-label='Text field to review a product']");
+  public static By SUBMIT_BUTTON = css("button[id='submitButton']");
 
   // Public methods ----------------------------------------------------------------------------------------------------
   public void waitForPageToLoad() {
--- src/test/java/com/testing/pageObjects/pages/RegisterPage.java
+++ src/test/java/com/testing/pageObjects/pages/RegisterPage.java
@@ -14,15 +14,15 @@ import net.thucydides.core.annotations.DefaultUrl;
 public class RegisterPage extends TopMenuSection {
   // Locators
   // -------------------------------------------------------------------------------------------------------------------
-  public static By USER_REGISTRATION_TITLE;
-  public static By EMAIL_FIELD;
-  public static By PASSWORD_FIELD;
-  public static By REPEAT_PASSWORD_FIELD;
-  public static By SECURITY_QUESTION;
-  public static By NAME_OF_FAVORITE_PET_OPTION;
-  public static By SECURITY_QUESTION_LIST;
-  public static By SECURITY_ANSWER_FIELD;
-  public static By REGISTRATION_BUTTON;
+  public static By USER_REGISTRATION_TITLE = xpath("//h1[contains(text(), 'User Registration')]");
+  public static By EMAIL_FIELD = css("input[id='emailControl']");
+  public static By PASSWORD_FIELD = css("input[id='passwordControl']");
+  public static By REPEAT_PASSWORD_FIELD = css("input[id='repeatPasswordControl']");
+  public static By SECURITY_QUESTION = css("mat-select[role='combobox']");
+  public static By NAME_OF_FAVORITE_PET_OPTION = xpath("//span[contains(text(), 'Name of your favorite pet?')]");
+  public static By SECURITY_QUESTION_LIST = css("mat-select[aria-label='Selection list for the security question']");
+  public static By SECURITY_ANSWER_FIELD = css("input[id='securityAnswerControl']");
+  public static By REGISTRATION_BUTTON = css("button[id='registerButton']");
 
   // Public methods
   // -------------------------------------------------------------------------------------------------------------------
